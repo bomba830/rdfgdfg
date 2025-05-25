b0403752058a52482af2e6f2a4a5a9c186da9ba4
bomba830 <girtins2009@gmail.com>
2025-05-25 13:22:03 +0300

    Add files via upload

---
+++ newFile.js
@@ -0,0 +1,4 @@
+<script>
+  function goToNextPage() {window.location.href = "otralapa.html" // This opens otralapa.html
+  }
+</script>;
---
+++ script.js
@@ -0,0 +1,6 @@
+  window.onload = function() {
+  alert("Sveicināts mūsu mājaslapā!");
+};
+
+ 
+    
